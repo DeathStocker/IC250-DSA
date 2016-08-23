ba995716881592aaa03dd146b13f42aebbc90db1
DeathStocker <utkarsh2602@gmail.com>
2016-08-23 23:12:05 +0530

    Started work on the solving function.

--- week2/iterative_hanoi.c
+++ week2/iterative_hanoi.c
@@ -8,6 +8,19 @@ void display_move(int count, char source, char dest, int disk)
 	printf("[%d] Move the disk %d from %c to %c\n", count, disk, source, dest);
 }
 
+int solve_hanoi(int disks, stack* src, stack* aux, stack* dest)
+{
+	char s = 'A', d = 'B', a = 'C';
+
+	/*
+	   Interchanging destination and auxillary if number of disks is even.
+	 */
+	if (disks % 2 == 0) {
+		char temp = d;
+		d = a;
+		a  = temp;
+	}
+
 int main()
 {
 	printf("Enter the number of disks = ");
