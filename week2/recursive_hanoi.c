16df208b5f541a4502ab1c1c82fe7315a3ddb7ec
DeathStocker <utkarsh2602@gmail.com>
2016-08-23 18:57:00 +0530

    Fixed segmentation fault due to no return in the if() block of recursion.

--- week2/recursive_hanoi.c
+++ week2/recursive_hanoi.c
@@ -4,6 +4,7 @@ int move_disk(int disk, char source, char dest, char aux)
 {
 	if(disk == 1) {
 		printf("Move disk %d from peg %c to peg %c.\n", disk, source, dest);
+		return 0;
 	}
 	move_disk(disk - 1, source, aux, dest);
 	printf("Move disk %d from peg %c to peg %c.\n", disk, source, dest);
