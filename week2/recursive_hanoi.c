f2113129580ffec92de8112479b4a8f6fa7af012
DeathStocker <utkarsh2602@gmail.com>
2016-08-23 19:15:38 +0530

    Added the final number of steps required to solve the Tower of Hanoi.

--- week2/recursive_hanoi.c
+++ week2/recursive_hanoi.c
@@ -2,15 +2,18 @@
 
 int move_disk(int disk, char source, char dest, char aux)
 {
+	int static count = 1;
 	if(disk == 1) {
-		printf("Move disk %d from peg %c to peg %c.\n", disk, source, dest);
-		return 0;
+		printf("[%d] Move disk %d from peg %c to peg %c.\n", count, disk, source, dest);
+		count++;
+		return count - 1;
 	}
 	move_disk(disk - 1, source, aux, dest);
-	printf("Move disk %d from peg %c to peg %c.\n", disk, source, dest);
+	printf("[%d] Move disk %d from peg %c to peg %c.\n", count, disk, source, dest);
+	count++;
 	move_disk(disk - 1, aux, dest, source);
 
-	return 0;
+	return count - 1;
 }
 
 int main()
@@ -19,7 +22,7 @@ int main()
 	int disk;
 	scanf("%d", &disk);
 
-	move_disk(disk, 'A', 'C', 'B');
-
+	int count = move_disk(disk, 'A', 'C', 'B');
+	printf("\nTotal number of steps required to solve this problem are = %d\n", count);
 	return 0;
 }
