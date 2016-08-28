599e51fb6122d4416a9e35dad7340ead780c5b9c
DeathStocker <utkarsh2602@gmail.com>
2016-08-28 18:57:00 +0530

    Added input(), process(), and transfer_stack() functions.

--- week3/queue_stack.c
+++ week3/queue_stack.c
@@ -1,5 +1,63 @@
 #include "stack.h"
 
+int transfer_stack(stack* src, stack* dest)
+{
+	while(is_empty(src) != -1) {
+		int val = pop(src);
+		push(dest, val);
+	}
+
+	return 0;
+}
+
+int process_queue(stack* queue, stack* temp, int ch)
+{
+
+	int val = 0;
+
+	switch (ch) {
+	case 1:
+		if (is_full(queue) == 1)
+			printf("Sorry the queue is full. Please pop/empty"
+			       " it before pushing.\n");
+		else {
+			printf("Enter the value to be pushed =");
+			scanf("%d", &val);
+			transfer_stack(queue, temp);
+			push(queue, val);
+			transfer_stack(temp, queue);
+		}
+
+	}
+
+	return 0;
+}
+
+int input(stack* queue, stack* temp)
+{
+	while (1) {
+		printf("\nChoose what you want to do with your queue -\n");
+		printf("\n 1 - Push");
+		printf("\n 2 - Pop");
+		printf("\n 3 - Top");
+		printf("\n 4 - Empty");
+		printf("\n 5 - Exit");
+		printf("\n 6 - Display");
+		printf("\n 7 - Stack Count");
+		printf("\n 8 - Destroy Stack");
+		printf("\n\nChoice = ");
+
+		int ch;
+		scanf("%d", &ch);
+
+		if(process_queue(queue, temp, ch) == 1)
+			break;
+		else
+			return 1;
+	}
+	return 0;
+}
+
 int main()
 {
 	printf("Enter the size of the queue = ");
@@ -14,26 +72,7 @@ int main()
 	stack* queue = create_stack(size);
 	stack* temp = create_stack(size);
 
-	printf("\nChoose what you want to do with your queue -\n");
-	printf("\n 1 - Push");
-	printf("\n 2 - Pop");
-	printf("\n 3 - Top");
-	printf("\n 4 - Empty");
-	printf("\n 5 - Exit");
-	printf("\n 6 - Display");
-	printf("\n 7 - Stack Count");
-	printf("\n 8 - Destroy Stack");
-	printf("\n\nChoice = ");
+	input(queue, temp);
 
-	int ch;
-	scanf("%d", &ch);
-
-	int val = 0;
-
-	switch(ch) {
-		case 1:
-		printf("Enter value to be pushed = ");
-		scanf("%d", &val);
-
-	}
+	return 0;
 }
