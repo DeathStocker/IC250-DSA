c029b48c7282c05d05f4808ef471a1eca1088279
DeathStocker <utkarsh2602@gmail.com>
2016-08-28 18:15:36 +0530

    Added the stack.c and stack.h files from Week - 2.

---
+++ week3/stack.c
@@ -0,0 +1,65 @@
+/*
+   Name - Utkarsh Kunwar
+   Roll No. - B15338
+   IC-250 Week - 3 (File taken from Week - 2)
+ */
+
+#include "stack.h"
+
+/*
+   Checks if the stack is full or not.
+ */
+int is_full(stack* s)
+{
+	if (s->top == (signed)s->capacity - 1)
+		return 1;
+	return 0;
+}
+
+/*
+   Checks if the stack is empty or not.
+ */
+int is_empty(stack* s)
+{
+	if (s->top == -1)
+		return 1;
+	return 0;
+}
+
+/*
+   "Pushes" the given value to the top of the stack and changes the top.
+ */
+int push(stack *s, int value)
+{
+	if (is_full(s))
+		return -1;
+	s->arr[++s->top] = value;
+
+	return 0;
+}
+
+/*
+   "Pops" the topmost item from the stack.
+ */
+int pop(stack* s)
+{
+	if (is_empty(s) == 1)
+		return -1;
+	int value = s->arr[s->top];
+	s->top--;
+	return value;
+}
+
+/*
+   Creates a stack of the given capacity.
+ */
+stack* create_stack(unsigned capacity)
+{
+	stack* s = (stack*)malloc(sizeof(stack));
+
+	s->capacity = capacity;
+	s->top = -1;
+	s->arr = (int*)malloc(s->capacity * sizeof(int));
+
+	return s;
+}
---
+++ week3/stack.h
@@ -0,0 +1,33 @@
+/*
+   Name - Utkarsh Kunwar
+   Roll No. - B15338
+   IC-250 Week - 3 (File taken from Week - 2)
+ */
+
+#ifndef STACK_H
+#define STACK_H
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+/*
+   User defined variable for stack
+ */
+typedef struct _stack {
+	unsigned capacity;
+	int top;
+	int *arr;
+} stack;
+
+stack* create_stack(unsigned capacity);
+
+int is_full(stack* s);
+
+int is_empty(stack* s);
+
+int push(stack *s, int item);
+
+int pop(stack* s);
+
+#endif
