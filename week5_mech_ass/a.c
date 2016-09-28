ab280d0d003677cc5a6aff2fe3cd0d696db6a172
DeathStocker <utkarsh2602@gmail.com>
2016-09-28 15:52:01 +0530

    Added ALL THE THINGSSSS!!!
    Added time calculation for both methods, added plotting for both (x* vs theta,
    and N vs error) for both, and some other minor things. Meh.

--- week5_mech_ass/a.c
+++ week5_mech_ass/a.c
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <string.h>
 #include <time.h>
+#define clear() printf("\033[H\033[J")
 
 #define FILE_ERR 1
 
@@ -233,14 +234,25 @@ int print_to_file(int ch, double* theta, double* theta_exact, int N, double delt
 /*
    Calculates the amount of memory allocated in the heap.
  */
-void calc_size_heap(double* a, double* b, double* d, double** mat, double** aug,
-		    double* r, double* res, double* theta_exact, int N)
+void calc_size_heap(int ch, double* a, double* b, double* d, double** mat,
+		    double** aug, double* r, double* res, double* theta_exact,
+		    double* theta_thomas, int N)
 {
-	double space = sizeof(a) * (N - 1) + sizeof(b) * (N - 1) + sizeof(d) * N
-		       + sizeof(mat) * (N * N) + sizeof(aug) * (N * (N + 1))
-		       + sizeof(res) * N + sizeof(r) * N + sizeof(theta_exact) * N;
+	double space;
 
-	printf("Space used = %.3lf KB\n", space / 1000);
+	switch (ch) {
+	case 0:
+		space = sizeof(mat) * (N * N) + sizeof(aug) * (N * (N + 1))
+			+ sizeof(res) * N + sizeof(theta_exact) * N + sizeof(r) * N;
+		break;
+	case 1:
+		space = sizeof(a) * (N - 1) + sizeof(b) * (N - 1) + sizeof(d) * N
+			+ sizeof(mat) * (N * N) + sizeof(r) * N
+			+ sizeof(theta_exact) * N + sizeof(theta_thomas) * N;
+	}
+
+	printf("Space used in %s = %.3lf KB\n", (ch == 0) ? "Gauss" : "Thomas",
+	       space / 1000);
 }
 
 double calc_theta_error(double* theta, double* theta_exact, int N)
@@ -249,7 +261,7 @@ double calc_theta_error(double* theta, double* theta_exact, int N)
 	int i;
 
 	for (i = 0; i < N; i++)
-		theta_error = pow(theta_exact[i] - theta[i], 2);
+		theta_error += pow(theta_exact[i] - theta[i], 2);
 	theta_error /= N;
 	theta_error = sqrt(theta_error);
 	return theta_error;
@@ -267,11 +279,58 @@ double* calc_theta_Thomas(double* a, double* b, double* d, double* r, int N)
 	double* theta = calloc(N, sizeof(double));
 	theta[N - 1] = r[N - 1] / d[N - 1];
 
-	for (i = N - 2; i > 0; i--)
+	// display_array(r, N);
+	// display_array(d, N);
+
+	for (i = N - 2; i >= 0; i--)
 		theta[i] = (r[i] - a[i] * theta[i + 1]) / d[i];
-	theta[0] = 1.0;
 
 	return theta;
+}
+
+/*
+   To plot the obtained data and output them to image files
+ */
+int plot()
+{
+	/*
+	   Commands for gnuplot
+	 */
+	char* plottingA[] = { "set terminal png",
+			      "set title \"Gauss\"",
+			      "set output 'gauss.png'",
+			      "plot 'results_gauss.dat' using 1:2 title 'Theta_calc' with line, 'results_gauss.dat' using 1:3 title 'Theta_exact' with line linecolor rgb 'black'" };
+
+	char* plottingB[] = { "set terminal png",
+			      "set title \"Thomas\"",
+			      "set output 'thomas.png'",
+			      "plot 'results_thomas.dat' using 1:2 title 'Theta_calc' with line, 'results_thomas.dat' using 1:3 title 'Theta_exact' with line linecolor rgb 'black'" };
+	char* plottingC[] = { "set terminal png",
+			      "set title \"Gauss\"",
+			      "set output 'gauss_error.png'",
+			      "plot 'NvsErr_Gauss.dat' using 1:2 title 'Error' with line" };
+	char* plottingD[] = { "set terminal png",
+			      "set title \"Thomas\"",
+			      "set output 'thomas_error.png'",
+			      "plot 'NvsErr_Thomas.dat' using 1:2 title 'Error' with line" };
+
+	int i;
+	FILE* gnuplotPipe = popen("gnuplot -persistent", "w");
+
+	/*
+	   Piping outputs to GNUPlot for saving the different graphs in
+	   portable network graphics files.
+	 */
+	for (i = 0; i < 4; i++)
+		fprintf(gnuplotPipe, "%s \n", plottingA[i]);
+	for (i = 0; i < 4; i++)
+		fprintf(gnuplotPipe, "%s \n", plottingB[i]);
+	for (i = 0; i < 4; i++)
+		fprintf(gnuplotPipe, "%s \n", plottingC[i]);
+	for (i = 0; i < 4; i++)
+		fprintf(gnuplotPipe, "%s \n", plottingD[i]);
+	pclose(gnuplotPipe);
+	return 0;
 }
 
 int main()
@@ -279,8 +338,16 @@ int main()
 	printf("Enter the value of N = ");
 	int N;
 	scanf("%d", &N);
-	double beta = 100.0;
-	printf("Enter the value of beta (Fixed) = %d\n", (int)beta);
+	if (N <= 1) {
+		printf("Value of N is cannot be less than or equal to 1.\n");
+		return err_calloc();
+	}
+	double beta;
+	printf("Enter the value of beta (Fixed) = ");
+	scanf("%lf", &beta);
+
+	// Clears the screen.
+	clear();
 
 	double delta_x = 1.0 / (N - 1);
 
@@ -293,27 +360,69 @@ int main()
 	double* b = calloc((N - 1), sizeof(double));
 	double* d = calloc(N, sizeof(double));
 	double* r = calloc(N, sizeof(double));
+	if (a == NULL || b == NULL || d == NULL || r == NULL)
+		return err_calloc();
 
+	// Initializes the matrix A.
 	mat = init_matrix(mat, N, delta_x, beta, a, b, d, r);
 
 	if (isTriDiagonal(mat, N))
 		printf("Matrix is tridiagonal.\n");
 
+	printf("\n\n");
+
+	clock_t t;
+	t = clock();
+	// Creates an augmented matrix from matrix A and vector V.
 	double** aug = create_aug_matrix(mat, r, N, N + 1);
 
+	// Performs Gauss elimination and returns the resultant vector.
 	double* res = gauss_eliminate(aug, N);
 
+	t = clock() - t;
+	double time_taken = ((double)t) / CLOCKS_PER_SEC * 1000;
+	printf("Time taken in Gauss = %lfms\n", time_taken);
+
+	// Calculates the exact value of theta.
 	double* theta_exact = calc_theta_exact(delta_x, beta, N);
 
+	// Prints to file results_gauss.dat.
 	print_to_file(0, res, theta_exact, N, delta_x);
 
-	calc_size_heap(a, b, d, mat, aug, r, res, theta_exact, N);
+	// Calculates the size allocated in the heap for Gauss method.
+	calc_size_heap(0, a, b, d, mat, aug, r, res, theta_exact, NULL, N);
 
-	double error = calc_theta_error(res, theta_exact, N);
-	printf("Error(theta, theta_exact) = %lf\n", error);
+	// Displays the error of the
+	double error_gauss = calc_theta_error(res, theta_exact, N);
+	printf("Error(theta, theta_exact) in Gauss = %lf\n", error_gauss);
 
+	printf("\n\n");
+
+	t = clock();
+	// Calculates and prints to file using Thomas algorithm.
 	double* theta_thomas = calc_theta_Thomas(a, b, d, r, N);
+
+	t = clock() - t;
+	time_taken = ((double)t) / CLOCKS_PER_SEC * 1000;
+	printf("Time taken in Thomas = %lfms\n", time_taken);
+
 	print_to_file(1, theta_thomas, theta_exact, N, delta_x);
+
+	// Calculates the size allocated in the heap for Thomas method.
+	calc_size_heap(1, a, b, d, NULL, NULL, r, NULL, theta_exact, theta_thomas, N);
+
+	// Displays the error of the
+	double error_thomas = calc_theta_error(theta_thomas, theta_exact, N);
+	printf("Error(theta, theta_exact) in Thomas = %lf\n", error_thomas);
+
+	// Prints the N vs Error table.
+	FILE* fp1 = fopen("NvsErr_Gauss.dat", "a");
+	fprintf(fp1, "%d\t%lf\n", N, error_gauss);
+	FILE* fp2 = fopen("NvsErr_Thomas.dat", "a");
+	fprintf(fp2, "%d\t%lf\n", N, error_thomas);
+
+	// Plots the graphs between x* and theta.
+	plot();
 
 	// Freeing all the pointers.
 	free(res);
@@ -325,6 +434,8 @@ int main()
 	free(r);
 	free_matrix(aug, N, N + 1);
 	free(theta_thomas);
+	fclose(fp1);
+	fclose(fp2);
 
 	return 0;
 }
