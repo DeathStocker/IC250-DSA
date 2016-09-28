286f1febb54371d1267d24d1543348eb525900f1
DeathStocker <utkarsh2602@gmail.com>
2016-09-28 14:21:10 +0530

    Added Thomas algorithm and theta_error calculation.

--- week5_mech_ass/a.c
+++ week5_mech_ass/a.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
+#include <time.h>
 
 #define FILE_ERR 1
 
@@ -200,19 +202,25 @@ double* calc_theta_exact(double delta_x, double beta, int N)
 	return theta_exact;
 }
 
-
-int print_to_file(double* theta, double* theta_exact, int N, double delta_x)
+/*
+   Prints the data to a file "results.dat"
+ */
+int print_to_file(int ch, double* theta, double* theta_exact, int N, double delta_x)
 {
 	int i;
 	double count = 0.0;
+	char str[16] = "results";
 
-	FILE* fp = fopen("results.dat", "w");
+	strcat(str, (ch == 0) ? "_gauss" : "_thomas");
+	strcat(str, ".dat");
+
+	FILE* fp = fopen(str, "w");
 
 	if (fp == NULL) {
 		printf("Cannot open file for writing. Exitting.\n");
 		return FILE_ERR;
 	}
-	fprintf(fp, "#delta_x  theta   theta_exact\n\n");
+	fprintf(fp, "#delta_x  theta_%s   theta_exact\n\n", (ch == 0) ? "_gauss" : "_thomas");
 
 	for (i = 0; i < N; i++, count += delta_x)
 		fprintf(fp, "%lf %lf %lf\n", count, theta[i], theta_exact[i]);
@@ -222,6 +230,9 @@ int print_to_file(double* theta, double* theta_exact, int N, double delta_x)
 	return 0;
 }
 
+/*
+   Calculates the amount of memory allocated in the heap.
+ */
 void calc_size_heap(double* a, double* b, double* d, double** mat, double** aug,
 		    double* r, double* res, double* theta_exact, int N)
 {
@@ -230,6 +241,37 @@ void calc_size_heap(double* a, double* b, double* d, double** mat, double** aug,
 		       + sizeof(res) * N + sizeof(r) * N + sizeof(theta_exact) * N;
 
 	printf("Space used = %.3lf KB\n", space / 1000);
+}
+
+double calc_theta_error(double* theta, double* theta_exact, int N)
+{
+	double theta_error = 0.0;
+	int i;
+
+	for (i = 0; i < N; i++)
+		theta_error = pow(theta_exact[i] - theta[i], 2);
+	theta_error /= N;
+	theta_error = sqrt(theta_error);
+	return theta_error;
+}
+
+double* calc_theta_Thomas(double* a, double* b, double* d, double* r, int N)
+{
+	int i;
+
+	for (i = 1; i < N; i++) {
+		d[i] -= (a[i - 1] * b[i - 1] / d[i - 1]);
+		r[i] -= (r[i - 1] * b[i - 1] / d[i - 1]);
+	}
+
+	double* theta = calloc(N, sizeof(double));
+	theta[N - 1] = r[N - 1] / d[N - 1];
+
+	for (i = N - 2; i > 0; i--)
+		theta[i] = (r[i] - a[i] * theta[i + 1]) / d[i];
+	theta[0] = 1.0;
+
+	return theta;
 }
 
 int main()
@@ -257,27 +299,21 @@ int main()
 	if (isTriDiagonal(mat, N))
 		printf("Matrix is tridiagonal.\n");
 
-	//printf("Matrix = \n");
-	//display_matrix(mat, N, N);
-
 	double** aug = create_aug_matrix(mat, r, N, N + 1);
-
-	//printf("Augmented Matrix = \n");
-	//display_matrix(aug, N, N + 1);
 
 	double* res = gauss_eliminate(aug, N);
 
-	//printf("Results =\n");
-	//display_array(res, N);
-
-
 	double* theta_exact = calc_theta_exact(delta_x, beta, N);
-	//printf("Analytical =\n");
-	//display_array(theta_exact, N);
 
-	print_to_file(res, theta_exact, N, delta_x);
+	print_to_file(0, res, theta_exact, N, delta_x);
 
 	calc_size_heap(a, b, d, mat, aug, r, res, theta_exact, N);
+
+	double error = calc_theta_error(res, theta_exact, N);
+	printf("Error(theta, theta_exact) = %lf\n", error);
+
+	double* theta_thomas = calc_theta_Thomas(a, b, d, r, N);
+	print_to_file(1, theta_thomas, theta_exact, N, delta_x);
 
 	// Freeing all the pointers.
 	free(res);
@@ -288,6 +324,7 @@ int main()
 	free(d);
 	free(r);
 	free_matrix(aug, N, N + 1);
+	free(theta_thomas);
 
 	return 0;
 }
